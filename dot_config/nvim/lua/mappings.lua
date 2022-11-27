7fee746d07a4e47c379bc01051eaf877b9c47e65
Balint Orosz <orosz.balint.97@gmail.com>
2022-11-27 21:31:07 +0100

    Update .config/nvim/lua/mappings.lua

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -1,15 +1,15 @@
 -- Utils
 local function bind(mode, options)
-	options = options or { noremap = true }
+  options = options or { noremap = true }
 
-	return function(lhs, rhs, opts)
-		opts = vim.tbl_extend("force",
-			options,
-			opts or {}
-		)
+  return function(lhs, rhs, opts)
+    opts = vim.tbl_extend("force",
+      options,
+      opts or {}
+    )
 
-		vim.keymap.set(mode, lhs, rhs, opts)
-	end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
 end
 
 -- local nmap = bind("n", { noremap = false })
@@ -22,38 +22,38 @@ local vnoremap = bind("v")
 local builtin = require('telescope.builtin')
 
 nnoremap("<C-p>", function()
-	builtin.find_files()
+  builtin.find_files()
 end)
 
 nnoremap("<leader>ff", function()
-	builtin.find_files()
+  builtin.find_files()
 end)
 
 nnoremap("<leader>fg", function()
-	builtin.live_grep()
+  builtin.live_grep()
 end)
 
 nnoremap("<leader>fb", function()
-	builtin.buffers()
+  builtin.buffers()
 end)
 
 
 nnoremap("<leader>fh", function()
-	builtin.help_tags()
+  builtin.help_tags()
 end)
 
 -- move commands
 
-nnoremap("<A-j>", function()
-	vim.cmd [[m .+1<CR>==]]
+nnoremap("<C-j>", function()
+  vim.cmd [[m .+1<CR>==]]
 end)
 
-nnoremap("<A-k>", function()
-	vim.cmd [[m .-2<CR>==]]
+nnoremap("<C-k>", function()
+  vim.cmd [[m .-2<CR>==]]
 end)
 
-vnoremap("<A-j>", ":m '>+1<CR>==gv")
-vnoremap("<A-k>", ":m '<-2<CR>==gv")
+vnoremap("<C-j>", ":m '>+1<CR>==gv")
+vnoremap("<C-k>", ":m '<-2<CR>==gv")
 
 -- Horizontal shift
 
@@ -66,24 +66,32 @@ vnoremap("<", "<gv")
 -- Netrw
 
 nnoremap("<leader>r", function()
-	if pcall(vim.cmd, "Rex") then
-	else
-		vim.cmd [[Ex]]
-	end
+  if pcall(vim.cmd, "Rex") then
+  else
+    vim.cmd [[Ex]]
+  end
 end)
 
 nnoremap("<leader>e", function()
-	vim.cmd [[Ex]]
+  vim.cmd [[Ex]]
 end)
 
 -- Git
 
 nnoremap("<leader>gs", function()
-	require("neogit").open({ kind = "replace" })
+  require("neogit").open({ kind = "replace" })
 end)
 
 nnoremap("<leader>gh", function()
-	vim.cmd [[Gitsigns preview_hunk]]
+  vim.cmd [[Gitsigns preview_hunk]]
+end)
+
+nnoremap("<leader>gn", function()
+  vim.cmd [[Gitsigns next_hunk]]
+end)
+
+nnoremap("<leader>gp", function()
+  vim.cmd [[Gitsigns prev_hunk]]
 end)
 
 -- Harpoon
@@ -92,21 +100,21 @@ local harpoon_ui = require("harpoon.ui")
 nnoremap("<leader>hh", function() require("harpoon.mark").add_file() end)
 nnoremap("<leader>ha", function() harpoon_ui.toggle_quick_menu() end)
 
-nnoremap("<A-1>", function() harpoon_ui.nav_file(1) end)
-nnoremap("<A-2>", function() harpoon_ui.nav_file(2) end)
-nnoremap("<A-3>", function() harpoon_ui.nav_file(3) end)
-nnoremap("<A-4>", function() harpoon_ui.nav_file(4) end)
+nnoremap("<leader>h1", function() harpoon_ui.nav_file(1) end)
+nnoremap("<leader>h2", function() harpoon_ui.nav_file(2) end)
+nnoremap("<leader>h3", function() harpoon_ui.nav_file(3) end)
+nnoremap("<leader>h4", function() harpoon_ui.nav_file(4) end)
 
 -- LSP
 
 nnoremap("<leader>pp", function()
-	vim.lsp.buf.format()
+  vim.lsp.buf.format()
 end)
 
 -- Other
 
 nnoremap("<leader>l", function()
-	vim.cmd [[nohlsearch]]
+  vim.cmd [[nohlsearch]]
 end)
 
 
