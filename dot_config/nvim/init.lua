d3763a66b6415b1f9066e675a3e31018ae32d7f8
Balint Orosz <orosz.balint.97@gmail.com>
2022-11-27 20:55:20 +0100

    Add correct order for init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,9 +2,14 @@ vim.defer_fn(function()
 	pcall(require, "impatient")
 end, 0)
 
-require "autocmds"
-require "lsp"
-require "mappings"
+-- options should be first
 require "options"
+
+-- plugins second
 require "plugins"
+
+-- then everything else
+require "mappings"
+require "lsp"
+require "autocmds"
 
